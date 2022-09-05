ec97f4266374315b82d85766aec2e576d06f981e
Alamgir kabir <alamgir.saguli@gmail.com>
2022-09-05 10:52:31 +0600

    3nd commit

---
+++ add-multiply.js
@@ -0,0 +1,24 @@
+/*
+chairWood = 3cft/chair
+tableWood = 10cft/table
+bedWood  = 50cft/bed
+
+*/
+
+function woodCalculator(chairQuantity, tableQuantity, bedQuantity){
+    const perChairWood =3;
+    const  perTableWood = 10;
+    const perBedWood  = 50;
+
+    // wood calculator
+    const chairWoodQuantity = chairQuantity * perChairWood;
+    const tableWoodQuantity = tableQuantity * perTableWood;
+    const bedWoodQuantity = bedQuantity * perBedWood;
+
+    const totalWood = chairWoodQuantity + tableWoodQuantity + bedWoodQuantity;
+    return totalWood;
+}
+const firstOption = woodCalculator(4, 2, 3);
+console.log(firstOption);
+
+// [{(Alhadullah)}]
